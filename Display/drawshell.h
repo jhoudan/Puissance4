dc9d271778c2c315121c6c3617a1efee6f89c560
Maxime DUMOULIN <mdumouli@e1r2p6.42.fr>
2016-02-27 15:31:33 +0100

    Updated var names

--- Display/drawshell.h
+++ Display/drawshell.h
@@ -6,15 +6,15 @@
 /*   By: mdumouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/27 14:40:03 by mdumouli          #+#    #+#             */
-/*   Updated: 2016/02/27 15:21:23 by mdumouli         ###   ########.fr       */
+/*   Updated: 2016/02/27 15:29:39 by mdumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DRAWSHELL_H
 # define DRAWSHELL_H
-# include "libft.h"
+# include "puissance.h"
 # define R_D "\e[31mo\e[0m"
 # define Y_D "\e[33mo\e[0m"
 
-void draw_border(t_grid grid, int nb);
+void draw_border(t_game game, int nb);
 #endif
