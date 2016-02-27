58d9a424e5839a2a8b959b4d9a6eb290d7aee604
Jerome HOUDAN <jerom.houdan@gmail.com>
2016-02-27 13:21:53 +0100

    creation of Check

--- includes/puissance.h
+++ includes/puissance.h
@@ -6,7 +6,7 @@
 /*   By: mdezitte <mdezitte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 20:12:46 by mdezitte          #+#    #+#             */
-/*   Updated: 2016/02/27 12:44:45 by mdezitte         ###   ########.fr       */
+/*   Updated: 2016/02/27 13:15:50 by jhoudan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ typedef struct	s_game
 }				t_game;
 
 int				game_core(t_game *game);
+
+int				get_input_player(t_game *game);
 
 #endif
---
+++ srcs/Check/checker.c
@@ -0,0 +1,58 @@
+#include <puissance.h>
+
+static int	is_valid_input(int input, t_game *game)
+{
+	if (input < 0 || input >= game->column)// || game->grid[game->line-1][input] != 0)
+		return (0);
+	return (1);
+}
+
+static int	ft_isnumber(char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	if (!*str)
+		return (0);
+	while (*str)
+	{
+		if (!ft_isdigit(*str) && *str != '\n')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+static int	print_grid_limit(t_game *game)
+{
+	ft_putstr("Please pick a column between 0 and ");
+	ft_putnbr(game->column - 1);
+	ft_putstr(": ");
+	return (1);
+}
+
+int			get_input_player(t_game *game)
+{
+	int		input;
+	char	*line;
+
+	line = NULL;
+	ft_putstr("Pick a column: ");
+	while (42)
+	{
+		get_next_line(STDIN_FILENO, &line);
+		if (line && ft_isnumber(line))
+		{
+			input = ft_atoi(line);
+			if (is_valid_input(input, game))
+				break ;
+		}
+		ft_strdel(&line);
+		print_grid_limit(game);
+	}
+	if (line)
+		ft_strdel(&line);
+	ft_putstr("You picked ");
+	ft_putnbr(input);
+	ft_putchar('\n');
+	return (input);
+}
