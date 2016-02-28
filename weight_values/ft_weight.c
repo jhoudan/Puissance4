860554805fd2415b59a7c3576ab75b6c8d7326bf
jogarcia <jogarcia@student.42.fr>
2016-02-28 13:37:24 +0100

    save

--- weight_values/ft_weight.c
+++ weight_values/ft_weight.c
@@ -1,13 +1,12 @@
 #include "../includes/puissance.h"
 
-static int		reverse_player(int player)
-{
-	if (player == 1)
-		return (2);
-	if (player == 2)
-		return (1);
-	return (0);
-}
+static int		reverse_player(int player);
+
+/*
+**  	------------------------------------------------------
+**  	|						MAIN	        	         |
+**  	------------------------------------------------------
+*/
 
 int				ft_weight_pos(t_game *game, int column, int player)
 {
@@ -19,4 +18,39 @@ int				ft_weight_pos(t_game *game, int column, int player)
 	weight += ft_weight_player(game, player, column, line);
 	weight += ft_weight_not_player(game, reverse_player(player), column, line);
 	return (weight);
+}
+
+int		ft_conv(int value, int option)
+{
+	int		conv;
+
+	if (option == 1)
+	{
+		conv = (value == 1) ? 10 : 0;
+		conv = (value == 2) ? 100 : 0;
+		conv = (value == 3) ? 1000 : 0;
+		conv = (value >= 4) ? 100000 : 0;
+	}
+	if (option == 2)
+	{
+		conv = (value == 1) ? 45 : 0;
+		conv = (value == 2) ? 1900 : 0;
+		conv = (value >= 3) ? 10000 : 0;
+	}
+	return (conv);
+}
+
+/*
+**  	------------------------------------------------------
+**  	|						Static	        	         |
+**  	------------------------------------------------------
+*/
+
+static int		reverse_player(int player)
+{
+	if (player == 1)
+		return (2);
+	if (player == 2)
+		return (1);
+	return (0);
 }
