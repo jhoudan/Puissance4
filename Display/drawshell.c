f6d425dd2e2250c432f2f0fa1ac145f135c220db
Mathias DEZITTER <mdezitte@e1r3p4.42.fr>
2016-02-28 13:03:38 +0100

    lol

--- Display/drawshell.c
+++ Display/drawshell.c
@@ -17,10 +17,10 @@ void draw_border(t_game game)
 	int x;
 
 	x = 1;
-	ft_putstr("\e[100m ");
+	ft_putstr("\033[100m ");
 	while (x++ < (game.column * 3))
 		ft_putchar(' ');
-	ft_putstr(" \e[0m\n");
+	ft_putstr(" \033[0m\n");
 }
 
 void display_col(int col)
@@ -30,13 +30,13 @@ void display_col(int col)
 	i = 0;
 	while (i < col)
 	{
-		ft_putstr("\e[100m \e[0m");
+		ft_putstr("\033[100m \033[0m");
 		if (i < 10)
 			ft_putchar('0');
 		ft_putnbr(i);
 		i++;
 	}
-	ft_putstr("\e[100m \e[0m");
+	ft_putstr("\033[100m \033[0m");
 	ft_putchar('\n');
 }
 
@@ -46,29 +46,29 @@ void draw_grid(t_game *game)
 	int y;
 
 	y = 0;
-	ft_putstr("\e[0m");
+	ft_putstr("\033[0m");
 	draw_border(*game);
 	while ((y++ / 2) < (*game).line)
 	{
 		x = 0;
-		((y / 2) < (*game).line) ? ft_putstr("\e[0;100m \e[0m") : 0;
+		((y / 2) < (*game).line) ? ft_putstr("\033[0;100m \033[0m") : 0;
 		while (x < (*game).column && y % 2 == 1)
 		{
 			if ((*game).grid[y / 2][x] == 1)
-				ft_putstr(RED_DOT);
+				ft_putstr("\033[0;41m  \033[0m");
 			else if ((*game).grid[y / 2][x] == 2)
-				ft_putstr(YELLOW_DOT);
+				ft_putstr("\033[0;43m  \033[0m");
 			else
-				ft_putstr("\e[0m  ");
-			(x == (*game).column - 1) ? 0 : ft_putstr("\e[100m \e[0m");
+				ft_putstr("\033[0m  ");
+			(x == (*game).column - 1) ? 0 : ft_putstr("\033[100m \033[0m");
 			++x;
 		}
 		while (x++ < (*game).column && y % 2 == 0 && (y / 2) < (*game).line)
 		{
-			ft_putstr("\e[100m  \e[0m");
-			(x == (*game).column) ? 0 : ft_putstr("\e[100m \e[0m");
+			ft_putstr("\033[100m  \033[0m");
+			(x == (*game).column) ? 0 : ft_putstr("\033[100m \033[0m");
 		}
-		((y / 2) < (*game).line) ? ft_putstr("\e[100m \e[0m\n") : 0;
+		((y / 2) < (*game).line) ? ft_putstr("\033[100m \033[0m\n") : 0;
 	}
 	draw_border(*game);
 	display_col(game->column);
